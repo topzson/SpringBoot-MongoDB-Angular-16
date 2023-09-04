ab10ac3f00141d6837e5e52c5fd1adfa3e7834ba
Rattatammanoon Prawasuk <toprattatammanoon@hotmail.com>
2023-09-04 10:21:02 +0700

    create springboot project and update Maven project

---
+++ SpringBoot-MongoDB/src/test/java/com/topzson_api/SpringBootMongoDB/SpringBootMongoDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.topzson_api.SpringBootMongoDB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMongoDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
