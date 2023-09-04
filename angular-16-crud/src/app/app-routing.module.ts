8101d00ebe21a0b4f22f40c9e94643fcb5af2ed5
Rattatammanoon Prawasuk <toprattatammanoon@hotmail.com>
2023-09-04 16:04:51 +0700

    setup app module about service model and router

--- angular-16-crud/src/app/app-routing.module.ts
+++ angular-16-crud/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddTutorialComponent } from './components/add-tutorial/add-tutorial.component';
+import { TutorialDetailsComponent } from './components/tutorial-details/tutorial-details.component';
+import { TutorialListComponent } from './components/tutorial-list/tutorial-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'tutorials', pathMatch: 'full' },
+  { path: 'tutorials', component: TutorialListComponent },
+  { path: 'tutorials/:id', component: TutorialDetailsComponent },
+  { path: 'add', component: AddTutorialComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
