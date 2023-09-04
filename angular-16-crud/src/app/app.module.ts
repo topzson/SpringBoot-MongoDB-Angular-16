40af60d8fee74c600b3c2b85adc0f69a00a7dfd4
Rattatammanoon Prawasuk <toprattatammanoon@hotmail.com>
2023-09-04 15:54:09 +0700

    generate some Components and Services

--- angular-16-crud/src/app/app.module.ts
+++ angular-16-crud/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddTutorialComponent } from './components/add-tutorial/add-tutorial.component';
+import { TutorialDetailsComponent } from './components/tutorial-details/tutorial-details.component';
+import { TutorialListComponent } from './components/tutorial-list/tutorial-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddTutorialComponent,
+    TutorialDetailsComponent,
+    TutorialListComponent
   ],
   imports: [
     BrowserModule,
