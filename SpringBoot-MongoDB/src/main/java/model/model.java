e43ea0a098c13fef220e47ace8e73981a859b7f0
Rattatammanoon Prawasuk <toprattatammanoon@hotmail.com>
2023-09-04 11:38:19 +0700

    Create,Retrieve,Update and Delete Operation

--- SpringBoot-MongoDB/src/main/java/model/model.java
+++ SpringBoot-MongoDB/src/main/java/model/model.java
@@ -1,2 +1,54 @@
-package model;public interface model {
+package model;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collation = "tutorials")
+public class model {
+    @Id
+    private String id;
+
+    private String title;
+    private String description;
+    private boolean published;
+
+    public model(){
+
+    }
+
+    public model(String title, String description, boolean published) {
+        this.title = title;
+        this.description = description;
+        this.published = published;
+    }
+
+    public String getId(){
+        return id;
+    }
+
+    public String getTitle(){
+        return title;
+    }
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description){
+        this.description = description;
+    }
+
+    public boolean isPublished(){
+        return published;
+    }
+
+    public void setPublished(boolean isPublished){
+        this.published = isPublished;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    @Override
+    public String toString(){
+        return "model [id=" + id + ", title=" + title + ", desc=" + description + ", published=" + published + "]";
+    }
 }
--- SpringBoot-MongoDB/src/main/java/repository/repository.java
+++ SpringBoot-MongoDB/src/main/java/repository/repository.java
@@ -1,2 +1,9 @@
-package repository;public class repository {
+package repository;
+import java.util.List;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import model.model;
+
+public interface repository extends MongoRepository<model, String> {
+    List<model> findByTitleContaining(String title);
+    List<model> findByPublished(boolean published);
 }
